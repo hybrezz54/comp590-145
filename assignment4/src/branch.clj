6756679e6a6ad8c76ec9ba8113fd9a1e9671c331
Hamzah Chaudhry <haz540@gmail.com>
2020-04-04 03:04:52 -0400

    Updated assignment 4

--- assignment4/src/branch.clj
+++ assignment4/src/branch.clj
@@ -2,13 +2,29 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]))
 
+(defn current-branch
+  "get the name of the current branch"
+  [dir db]
+  (let [db-path (str dir "/" db)
+        head-path (str db-path "/" "HEAD")]
+    (-> head-path slurp (str/split #"\n") first (str/split #"/") last)))
+
+(defn list-branches
+  "list the names of all branches"
+  [dir db]
+  (let [db-path (str dir "/" db)
+        branches (->> db-path io/file .listFiles (sort-by #(.getName %)))
+        current (current-branch db dir)]
+    (doseq [branch branches] (let [name (.getName branch)]
+                               (if (not= name current) (println (str "  " name))
+                                   (println (str "* " name)))))))
+
 (defn main
   "list or delete branches"
   [dir db n]
   (let [flag (first n)
         branch (second n)
         db-path (str dir "/" db)
-        head-path (str db-path "/" "HEAD")
         ref-path (str db-path "/refs/heads/" (if (= flag "-d") branch flag))]
     (try
       (cond (or (= flag "-h") (= flag "--help"))
@@ -19,14 +35,13 @@
                 (println "Arguments:")
                 (println "   -d <branch>   delete branch <branch>"))
             (and (= flag "-d") (nil? branch)) (throw (Exception.))
-            (or (and (not= flag "-d") (> (count n) 1)) (and (= flag "-d") (> (count n) 2))) (println "Error: invalid arguments.")
+            (or (and (not= flag "-d") (not (nil? flag))) (< 2 (count n))) (println "Error: invalid arguments.")
             (not (.exists (io/file db-path))) (println "Error: could not find database. (Did you run `idiot init`?)")
             (and (= flag "-d") (not (.exists (io/file ref-path)))) (println (format "Error: branch '%s' not found." branch))
-            (and (= flag "-d") (str/ends-with? (slurp head-path) branch)) (println (format "Error: cannot delete checked-out branch '%s'." branch))
-            (= flag "-d") (do (io/delete-file ref-path)
-                              (println (format "Deleted branch %s." branch)))
-            :else (do (println (format "ref: refs/heads/%s\n" flag))
-                      (println (format "Switched to branch '%s'" flag))))
+            (= flag "-d") (cond (= (current-branch dir db) branch) (println (format "Error: cannot delete checked-out branch '%s'." branch))
+                                :else (do (io/delete-file ref-path)
+                                          (println (format "Deleted branch %s." branch))))
+            :else (list-branches dir db))
 
       (catch Exception e
         e (println "Error: you must specify a branch name.")))))
